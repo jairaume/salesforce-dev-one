e32a694d4e9035b2c1b31ff74242046c5dfdd920
jairaume <rasclejerome@gmail.com>
2024-04-11 10:52:44 +0200

    fix: 🐛 fixed navigation handling

--- background.js
+++ background.js
@@ -1,85 +1,98 @@
 import themes from '../themes.json' with { type: "json" };
 const DEV_CONSOLE_URL = 'salesforce.com/_ui/common/apex/debug/ApexCSIPage';
 
-const currentState = {
+const initState = {
   active: false,
   themeId: 0,
   themes: themes
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ state: currentState })
+  chrome.storage.sync.set({ state: initState })
 });
 
 chrome.storage.onChanged.addListener(handleChange)
 
-chrome.tabs.onUpdated.addListener(handleChange)
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  console.log('Tab activated')
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    if(isCurrentTabAllowed(tab)){
+      applyCustomization(tab);
+    }
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
+  console.log('Tab updated')
+  if(isCurrentTabAllowed(tab)){
+    applyCustomization(tab);
+  }
+})
 
 async function handleChange(){
-    console.log('Storage changed')
-    const tab = await getCurrentTab();
-    chrome.storage.sync.get('state', (data) => {
-        if(data.state){
-            currentState.active = data.state.active;
-            currentState.themeId = data.state.themeId;
-        }
-        if(isCurrentTabAllowed(tab)){
-            applyCustomization(tab);
-        }
-    });
+  console.log('Storage changed')
+  const tab = await getCurrentTab();
+  if(isCurrentTabAllowed(tab)){
+    applyCustomization(tab);
+  }
 }
 
 function applyCustomization(tab) {
-    if(allowedUrl(tab.url)){
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: toggleTheme,
-            args: [currentState.active]
-        });
-        
-        chrome.scripting.executeScript({
-            target: { tabId: tab.id },
-            function: applyTheme,
-            args: [themes[currentState.themeId]]
-        });
-    }
+  if(allowedUrl(tab.url)){
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: toggleTheme,
+    });
+    
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: applyTheme,
+    });
+  }
 }
 
 // Activate or not the theming
-function toggleTheme(value) {
-  if(value){
-    console.log("Adding theme")
-    document.body.classList.add('dev-console-one-theme');
-  } else {
-    console.log("Removing theme")
-    document.body.classList.remove('dev-console-one-theme');
-  }
+function toggleTheme() {
+  chrome.storage.sync.get('state', ({ state }) => {
+    const { active } = state;
+    if(active){
+      console.log("Adding theme")
+      document.body.classList.add('dev-console-one-theme');
+    } else {
+      console.log("Removing theme")
+      document.body.classList.remove('dev-console-one-theme');
+    }
+  });
 }
 // Apply the selected theme colors
-function applyTheme(theme) {
-  const colorMap = new Map(Object.entries(theme.colors));
-  console.log("Setting theme colors ", theme.name)
-  for(const [key, value] of colorMap){
-    console.log(key, value)
-    document.documentElement.style.setProperty('--'+key, value);
-  };
+function applyTheme() {
+  chrome.storage.sync.get('state', ({ state }) => {
+    const { themeId, themes } = state;
+    const theme = themes[themeId];
+    const colorMap = new Map(Object.entries(theme.colors));
+    console.log("Setting theme colors ", theme.name)
+    for(const [key, value] of colorMap){
+      console.log(key, value)
+      document.documentElement.style.setProperty('--'+key, value);
+    };
+  });
 }
 
 
 // Returns true if the current tab is allowed
 async function isCurrentTabAllowed() {
-    const tab = await getCurrentTab();
-    return allowedUrl(tab.url);
+  const tab = await getCurrentTab();
+  return allowedUrl(tab.url);
 }
 
 // Return the current tab
 async function getCurrentTab() {
-    const queryOptions = { active: true, currentWindow: true };
-    const [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
 // Returns true if the url is a dev console url
 function allowedUrl(url) {
-  return url?.indexOf(DEV_CONSOLE_URL) !== -1;
+  return url?.startsWith('http') && url?.indexOf(DEV_CONSOLE_URL) !== -1;
 }
