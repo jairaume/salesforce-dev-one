a736971421bebda2b94d8bb68c04eb97547d0c36
jairaume <rasclejerome@gmail.com>
2024-04-17 11:57:05 +0200

    feat: 🎸 Added new side panel chackbox and style changes

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import { Icon } from '@iconify-icon/react';
 import { State } from "./types";
 import "./tailwind.css";
 
@@ -54,27 +55,25 @@ const Popup = () => {
   }
 
   return (
-    <main className="dark:bg-gray-950 dark:text-white p-3 px-5 grid gap-3">
+    <main className="dark:bg-gray-950 dark:text-neutral-200 p-3 px-5 grid gap-3 relative">
       <span className={"absolute top-0 left-0 -translate-x-1/3 -translate-y-1/3 w-1/2 h-1/2 blur-[80px] opacity-80 duration-500 pointer-events-none z-0 "+(state.active &&"bg-[--accent-color]")}>&nbsp;</span>
-      <h1 className="text-2xl">Salesforce <span className={"font-bold duration-300 "+(state.active && "text-[--accent-color]")}>Dev One</span></h1>
+      <h1 className="text-2xl z-10">Salesforce Dev <span className={"font-bold duration-300 "+(state.active && "text-[--accent-color]")}>One</span></h1>
       {
         isLoading ? 
           <p>Loading...</p> 
         : 
         (
-          <form className="grid gap-3">
+          <form className="grid gap-3 z-10 select-none">
             <div className="flex gap-3">
               <input type="checkbox" id="activeCheckbox" checked={state.active} onChange={handleActiveCheckbox}/>
-              <label className="text-base text-neutral-200" htmlFor="activeCheckbox">Theme</label>
-            </div>
-
-            <div className={"duration-300 flex gap-3 " + (!state.active && "opacity-50")}>
-              <input type="checkbox" disabled={!state.active} id="animationsCheckbox" checked={state.animations} onChange={handleAnimationsCheckbox}/>
-              <label className="text-base text-neutral-200" htmlFor="animationsCheckbox">Animations</label>
+              <label className="text-base " htmlFor="activeCheckbox">Active</label>
             </div>
 
             <div className={"duration-300 grid gap-1 " + (!state.active && "opacity-50")}>
-              <label className="text-base text-neutral-200" htmlFor="theme-select">Choose a theme</label>
+              <label className="text-base inline-flex items-center gap-2" htmlFor="theme-select">
+                <Icon icon="streamline:paintbrush-2-solid" className="text-sm"/>
+                <p>Choose a theme</p>
+              </label>
               <select 
                 className="rounded-md p-1 border-2 dark:bg-neutral-950 hover:dark:bg-neutral-800 dark:text-white dark:border-neutral-700 hover:dark:border-neutral-600 focus:dark:border-neutral-500 focus:outline-none duration-300"
                 id="theme" 
@@ -86,12 +85,36 @@ const Popup = () => {
               </select>
               <p className="dark:text-neutral-500 text-xs text-center">{state.themes[state.themeId]?.description}</p>
             </div>
+            
+            <div >
+              <h2 className="font-semibold dark:text-neutral-400">Additional options</h2>
+              
+              <div className="duration-300" style={{opacity: !state.active ? .5 : 1}}>
+                <div className="flex gap-3">
+                  <input type="checkbox" disabled={!state.active} id="animationsCheckbox" checked={state.animations} onChange={handleAnimationsCheckbox}/>
+                  <label className="text-base inline-flex items-center gap-2" htmlFor="animationsCheckbox">
+                    <Icon icon="material-symbols:animation"/>
+                    <p>Animations</p>
+                  </label>
+                </div>
+              </div>
+              
+              <div className="duration-300" style={{opacity: !state.active ? .5 : 1}}>
+                <div className="flex gap-3">
+                  <input type="checkbox" disabled={!state.active} id="animationsCheckbox" checked={state.animations} onChange={handleAnimationsCheckbox}/>
+                  <label className="text-base inline-flex items-center gap-2" htmlFor="animationsCheckbox">
+                    <Icon icon="fluent:panel-right-32-filled"/>
+                    <p>Side Panel</p>
+                  </label>
+                </div>
+              </div>
 
+            </div>
           </form>
         )
       }
-      <footer>
-        <p className="text-xs font-semibold dark:text-neutral-500">Made with ❤️ by <a title="Jairaume" href="https://www.rasclejero.me" className="underline hover:text-[--accent-color]">jairaume</a></p>
+      <footer className="mt-2">
+        <p className="text-[10px] text-center font-semibold dark:text-neutral-500">Made with ❤️ by <a title="Jairaume" href="https://www.rasclejero.me" className="underline hover:text-[--accent-color]">jairaume</a></p>
       </footer>
     </main>
   )
